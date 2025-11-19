e558944febad9dd8f7cb7300f76fc49c091f3d7e
istudormihai <istudor9@gmail.com>
2025-11-19 12:32:51 +0200

    Added 1st exercise of the 9th lab

---
+++ LAB9/assign1.py
@@ -0,0 +1,58 @@
+import random
+
+
+def random_dna(length):
+    return "".join(random.choice("ACGT") for _ in range(length))
+
+
+genome_length = random.randint(200, 400)
+genome = list(random_dna(genome_length))
+
+num_tes = random.randint(3, 4)
+
+transposons = [random_dna(random.randint(15, 25)) for _ in range(num_tes)]
+
+insert_positions = []
+
+pos = random.randint(0, len(genome) - 1)
+insert_positions.append(pos)
+genome[pos:pos] = list(transposons[0])
+
+pos2 = pos + random.randint(1, len(transposons[0]) - 5)
+insert_positions.append(pos2)
+genome[pos2:pos2] = list(transposons[1])
+
+if num_tes >= 3:
+    pos3 = pos + random.randint(3, len(transposons[0]) - 8)
+    insert_positions.append(pos3)
+    genome[pos3:pos3] = list(transposons[2])
+
+if num_tes == 4:
+    pos4 = random.randint(0, len(genome) - 1)
+    insert_positions.append(pos4)
+    genome[pos4:pos4] = list(transposons[3])
+
+genome = "".join(genome)
+
+detections = []
+
+for te in transposons:
+    start = 0
+    while True:
+        idx = genome.find(te, start)
+        if idx == -1:
+            break
+        detections.append((te, idx, idx + len(te)))
+        start = idx + 1  
+
+
+print("\nGENOME LENGTH AFTER INSERTION")
+print(len(genome))
+
+print("\nTRANSPOSONS (SOME OVERLAPPING)")
+for i, (te, pos) in enumerate(zip(transposons, insert_positions), 1):
+    print(f"TE{i}: {te}  | Inserted at index: {pos}")
+
+print("\nDETECTED TRANSPOSON POSITIONS")
+for te, start, end in detections:
+    print(f"Detected TE '{te}' from {start} to {end}")
